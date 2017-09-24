85614babbf4053e4a6b24a98a31c195456b7ebda
sharmajiKaLadka <32198992+sharmajiKaLadka@users.noreply.github.com>
2017-09-24 22:54:19 +0530

    adding sample string

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 //input string in Tag - Length - Value format (Sub Tags are supported)
 var inputStr =
-"";
+"0015SharmaJiKaLadka";
 // Options 
 // tagKeySize : number of digits for tagSize and lengthSize
 // example in '0004john' here tag is 00 length of value is 04, actual value is 'john' 
